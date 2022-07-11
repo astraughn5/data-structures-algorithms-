65e7c6d48b8c20f1b0e7caf693fbc321f4702772
astraughn5 <astraughn5@gmail.com>
2022-07-11 10:45:36 -0500

    adding practice

--- interviewProblem.js
+++ interviewProblem.js
@@ -230,6 +230,19 @@ function mergeSort (array){
 	)
 
 	function merge(left, right){
-		
+		const result = []
+		let leftIndex = 0
+		let rightIndex = 0
+		while( leftIndex < left.length && rightIndex < right.length){
+			if (left[leftIndex] < right[rightIndex]){
+				result.push(left[leftIndex])
+				leftIndex++
+			}
+			else {
+				result.push(right[rightIndex])
+				rightIndex++
+			}
+		}
+
 	}
 }
