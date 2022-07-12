cf063b3cbe9b45dccba10b76a3188e1dc174d870
astraughn5 <astraughn5@gmail.com>
2022-07-12 10:43:08 -0500

    adding practice

--- interviewProblem.js
+++ interviewProblem.js
@@ -111,6 +111,48 @@ function fibonnaciIterative(n){ //O(n)
 
 numbers = [5,2,100,9,26,5]
 
+function insertSort(array){
+	//starting at index 0, check if next value is less than
+	// swap if yes, stay if no
+	const length = array.length
+	for (let i = 0; i < length; i++){
+		if (array[i] < array[0]){
+			//move number to the first position
+			array.unshift(array.splice(i,1)[0])
+		} else {
+			//find where number should go
+			for (let j =1; j < i; j++){
+				if (array[i] > array[j-1] && array[i] < array[j]){
+					//move nuber to the right spot
+					array.splice(j,0,array.splice(i,1)[0])
+				}
+			}
+		}
+	}
+
+}
+//insertSort(numbers)
+
+//const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+ //better solution
+const insertionSort1 = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let j = i + 1;
+    while (array[j] < array[j - 1]) {
+      const temp = array[j];
+      array[j] = array[j-1];
+      array[j-1] = temp;
+      j--;
+    }
+  }
+  return array;
+};
+ 
+// insertionSort1(numbers);
+// console.log(numbers)
+
+
+
 function bubbleSort1(array){
 	const length = array.length
 	for (let i = 0; i < length; i++){
@@ -124,8 +166,33 @@ function bubbleSort1(array){
 		}
 	}
 }
-bubbleSort1(numbers)
-console.log(numbers)
+// bubbleSort1(numbers)
+// console.log(numbers)
+
+
+
+
+function selectionSort(array){
+	const length = array.length
+	for (let i = 0; i < length; i++){
+		//console.log(array)
+		let min = i
+		let temp = array[i]
+
+		for (let j = i+1; j < length; j++){
+			if (array[j] < array[min]){
+				min = j 
+			}
+		}
+
+		array[i] = array[min]
+		array[min] = temp
+	}
+return array
+}
+
+// selectionSort(numbers)
+// console.log(numbers)
 
 function bubbleSort(array){
 	let sort = []
@@ -143,3 +210,26 @@ function bubbleSort(array){
 	return sort
 }
 //console.log(bubbleSort(numbers))
+
+const numbers1 = [99,44,6,2,1,5,63,87,283,4,0]
+
+function mergeSort (array){
+	if (array.length === 1) {
+		return array
+	}
+
+	//split array into right and left
+	const length = array.length
+	const middle = Math.floor(length / 2)
+	const left = array.slice(0,middle)
+	const right = array.slice(middle,length)
+
+	return merge(
+		mergeSort(left),
+		mergeSort(right)
+	)
+
+	function merge(left, right){
+		
+	}
+}
