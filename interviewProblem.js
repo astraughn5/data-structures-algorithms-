3b8e323117c262f8374fb0ffa5399f4f572e8b67
astraughn5 <astraughn5@gmail.com>
2022-07-14 15:13:58 -0500

    adding practice

--- interviewProblem.js
+++ interviewProblem.js
@@ -248,8 +248,8 @@ function mergeSort(array){
 		return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
 	}
 }
-const answer = mergeSort(numbers1)
-console.log(answer)
+//const answer = mergeSort(numbers1)
+//console.log(answer)
 
 //quick sort
 const numbers2 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
@@ -329,3 +329,264 @@ function swap(array, firstIndex, secondIndex){
 //#8 - You want to teach sorting for the first time
 //answer - bubble sort or selection sort - help understand comparison sorting
 //class answer - bubble sort
+
+
+
+//BFS vs DFS
+
+//If you know a solution is not far from the root of the tree:
+//BFS - bc it starts at the root node
+
+//If the tree is very deep and solutions are rare: 
+//BFS bc DFS will take a long time in the deep section
+
+//If the tree is very wide:
+// DFS (BFS will need too much memory to keep track of all the nodes in queue)
+
+//If solutions are frequent but located deep in the tree:
+//DFS - better at diving deep first
+
+//Determining whether a path exists between two nodes:
+//DFS - good at answering does this path exist bc it follows the branches 
+
+//Finding the shortest path:
+//BFS 
+
+//
+class Node {
+	constructor(value){
+	  this.left = null;
+	  this.right = null;
+	  this.value = value;
+	}
+  }
+  
+  class BinarySearchTree {
+	constructor(){
+	  this.root = null;
+	}
+	insert(value){
+	  const newNode = new Node(value);
+	  if (this.root === null) {
+		this.root = newNode;
+	  } else {
+		let currentNode = this.root;
+		while(true){
+		  if(value < currentNode.value){
+			//Left
+			if(!currentNode.left){
+			  currentNode.left = newNode;
+			  return this;
+			}
+			currentNode = currentNode.left;
+		  } else {
+			//Right
+			if(!currentNode.right){
+			  currentNode.right = newNode;
+			  return this;
+			} 
+			currentNode = currentNode.right;
+		  }
+		}
+	  }
+	}
+	lookup(value){
+	  if (!this.root) {
+		return false;
+	  }
+	  let currentNode = this.root;
+	  while(currentNode){
+		if(value < currentNode.value){
+		  currentNode = currentNode.left;
+		} else if(value > currentNode.value){
+		  currentNode = currentNode.right;
+		} else if (currentNode.value === value) {
+		  return currentNode;
+		}
+	  }
+	  return null
+	}
+	remove(value) {
+	  if (!this.root) {
+		return false;
+	  }
+	  let currentNode = this.root;
+	  let parentNode = null;
+	  while(currentNode){
+		if(value < currentNode.value){
+		  parentNode = currentNode;
+		  currentNode = currentNode.left;
+		} else if(value > currentNode.value){
+		  parentNode = currentNode;
+		  currentNode = currentNode.right;
+		} else if (currentNode.value === value) {
+		  //We have a match, get to work!
+		  
+		  //Option 1: No right child: 
+		  if (currentNode.right === null) {
+			if (parentNode === null) {
+			  this.root = currentNode.left;
+			} else {
+			  
+			  //if parent > current value, make current left child a child of parent
+			  if(currentNode.value < parentNode.value) {
+				parentNode.left = currentNode.left;
+			  
+			  //if parent < current value, make left child a right child of parent
+			  } else if(currentNode.value > parentNode.value) {
+				parentNode.right = currentNode.left;
+			  }
+			}
+		  
+		  //Option 2: Right child which doesnt have a left child
+		  } else if (currentNode.right.left === null) {
+			currentNode.right.left = currentNode.left;
+			if(parentNode === null) {
+			  this.root = currentNode.right;
+			} else {
+			  
+			  //if parent > current, make right child of the left the parent
+			  if(currentNode.value < parentNode.value) {
+				parentNode.left = currentNode.right;
+			  
+			  //if parent < current, make right child a right child of the parent
+			  } else if (currentNode.value > parentNode.value) {
+				parentNode.right = currentNode.right;
+			  }
+			}
+		  
+		  //Option 3: Right child that has a left child
+		  } else {
+  
+			//find the Right child's left most child
+			let leftmost = currentNode.right.left;
+			let leftmostParent = currentNode.right;
+			while(leftmost.left !== null) {
+			  leftmostParent = leftmost;
+			  leftmost = leftmost.left;
+			}
+			
+			//Parent's left subtree is now leftmost's right subtree
+			leftmostParent.left = leftmost.right;
+			leftmost.left = currentNode.left;
+			leftmost.right = currentNode.right;
+  
+			if(parentNode === null) {
+			  this.root = leftmost;
+			} else {
+			  if(currentNode.value < parentNode.value) {
+				parentNode.left = leftmost;
+			  } else if(currentNode.value > parentNode.value) {
+				parentNode.right = leftmost;
+			  }
+			}
+		  }
+		return true;
+		}
+	  }
+	}
+	breadthFirstSearch(){
+		let currentNode = this.root;
+		let list = [];
+		let queue = [];
+		queue.push(currentNode);
+
+		while (queue.length > 0){
+			currentNode = queue.shift();
+			list.push(currentNode.value);
+			if (currentNode.left){
+				queue.push(currentNode.left)
+			}
+			if (currentNode.right){
+				queue.push(currentNode.right);
+			}
+		}
+		console.log(list)
+		return list
+	}
+	breadthFirstSearchRecursive(queue,list){
+		if (!queue.length){
+			return list;
+		}
+		let currentNode = queue.shift();
+		list.push(currentNode.value);
+		if (currentNode.left){
+			queue.push(currentNode.left)
+		}
+		if (currentNode.right){
+			queue.push(currentNode.right);
+		}
+		return this.breadthFirstSearchRecursive(queue, list)
+	}
+	DFSInOrder(){
+		return traverseInOrder(this.root, [])
+	}
+
+	DFSPreOrder(){
+		return traversePreOrder(this.root, [])
+	}
+
+	DFSPostOrder(){
+		return traversePostOrder(this.root, [])
+	}
+
+	
+  }
+
+  function traverseInOrder(node, list){
+	if (node.left){
+		traverseInOrder(node.left, list)
+	}
+	list.push(node.value)
+	if (node.right){
+		traverseInOrder(node.right, list)
+	}
+	return list;
+}
+function traversePreOrder(node, list){
+	list.push(node.value)
+	if (node.left){
+		traversePreOrder(node.left, list)
+	}
+	if (node.right){
+		traversePreOrder(node.right, list)
+	}
+	return list;
+}
+function traversePostOrder(node, list){
+	if (node.left){
+		traversePostOrder(node.left, list)
+	}
+	if (node.right){
+		traversePostOrder(node.right, list)
+	}
+	list.push(node.value)
+	return list;
+}
+   //     9
+  //  4     20
+  //1  6  15  170
+  const tree = new BinarySearchTree();
+  tree.insert(9)
+  tree.insert(4)
+  tree.insert(6)
+  tree.insert(20)
+  tree.insert(170)
+  tree.insert(15)
+  tree.insert(1)
+  tree.remove(170)
+  JSON.stringify(traverse(tree.root))
+ // tree.breadthFirstSearch()
+  //console.log(tree.breadthFirstSearchRecursive([tree.root],[]))
+  console.log(tree.DFSInOrder())
+  console.log(tree.DFSPreOrder())
+  console.log(tree.DFSPostOrder())
+ 
+  
+  function traverse(node) {
+	const tree = { value: node.value };
+	tree.left = node.left === null ? null : traverse(node.left);
+	tree.right = node.right === null ? null : traverse(node.right);
+	return tree;
+  }
+  
